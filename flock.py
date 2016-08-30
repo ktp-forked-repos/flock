fa4d2fa39faedafb3074f586a915ca8f606d85de
Kiran Tomlinson <kirantomlinson@gmail.com>
2016-08-29 19:25:40 -0500

    refactoring and gitingore change

--- flock.py
+++ flock.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 import math
 import random
 
@@ -7,10 +6,10 @@ import random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-RADIUS = 200
+RADIUS = 500
 
 
-def pointInCircle(x, y, radius):
+def point_in_circle(x, y, radius):
     dx = abs(x - radius)
     dy = abs(y - radius)
 
@@ -23,21 +22,19 @@ def pointInCircle(x, y, radius):
     if dy > radius:
         return False
 
-    if dx * dx + dy * dy > radius * radius:
+    if dx*dx + dy*dy > radius * radius:
         return False
     else:
         return True
 
-        
-    
-class Bird:
 
+class Bird:
     x = 0
     y = 0
     speed = 0
     heading = 0
     color = ()
-    
+
     def __init__(self, x, y, speed, heading, color):
         self.x = x
         self.y = y
@@ -45,49 +42,46 @@ class Bird:
         self.heading = heading
         self.color = color
 
-    def updateHeading(self):        
-       
+    def update_heading(self):
+
         if random.randint(0, 3) == 3:
             delta = random.randint(-10, 10) / 30
             self.heading += delta
 
-        projX = self.x + 30*math.sin(self.heading)
-        projY = self.y - 30*math.cos(self.heading)
-        
-        while not pointInCircle(projX, projY, RADIUS):
+        proj_x = self.x + 30 * math.sin(self.heading)
+        proj_y = self.y - 30 * math.cos(self.heading)
+
+        while not point_in_circle(proj_x, proj_y, RADIUS):
             self.heading -= 0.15
-            projX = self.x + 30*math.sin(self.heading)
-            projY = self.y - 30*math.cos(self.heading)
-        
+            proj_x = self.x + 30 * math.sin(self.heading)
+            proj_y = self.y - 30 * math.cos(self.heading)
 
     def move(self):
-        self.updateHeading()
+        self.update_heading()
 
-        self.x += self.speed * math.sin(self.heading)  
+        self.x += self.speed * math.sin(self.heading)
         self.y -= self.speed * math.cos(self.heading)
 
-def main(args):
-    
-    
+
+def main():
     # Initialize pygame
     pygame.init()
     clock = pygame.time.Clock()
-    
+
     # Window size
     size = (RADIUS * 2, RADIUS * 2)
     screen = pygame.display.set_mode(size)
 
     # Window title
-    pygame.display.set_caption('pysim')
+    pygame.display.set_caption('Flock')
 
     # Sim variables
     tweety = Bird(RADIUS, RADIUS, 3, 0, RED)
 
     flock = [tweety]
-    for i in range(50):
+    for i in range(200):
         color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         flock.append(Bird(RADIUS, RADIUS, 1 + (2 * random.random()), 2 * random.random() * math.pi, color))
-        
 
     # Main sim loop
     done = False
@@ -95,35 +89,32 @@ def main(args):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-        
+
         # Clear screen
         screen.fill(BLACK)
         pygame.draw.ellipse(screen, WHITE, [0, 0, RADIUS * 2, RADIUS * 2])
 
         # Move and draw birds
         for bird in flock:
-
             # Move bird
             bird.move()
 
-            #Draw bird
+            # Draw bird
             x = int(bird.x)
             y = int(bird.y)
-            #hx = int(x + 30*math.sin(bird.heading))
-            #hy = int(y - 30*math.cos(bird.heading))
+            # hx = int(x + 30*math.sin(bird.heading))
+            # hy = int(y - 30*math.cos(bird.heading))
             pygame.draw.circle(screen, bird.color, [x, y], 5)
-            #pygame.draw.line(screen, RED, [x, y], [hx, hy], 2)
-            
+            # pygame.draw.line(screen, RED, [x, y], [hx, hy], 2)
 
-    
         # Update screen    
         pygame.display.flip()
 
         # Limit to 60 fps
         clock.tick(60)
 
-
     pygame.quit()
-        
+
+
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
